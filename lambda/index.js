c32660f45789e4f0695dba23999cdac575220142
Francisco González Ibáñez <fragonib@gmail.com>
2024-08-29 18:50:26 +0200

    Initial commit

---
+++ lambda/index.js
@@ -0,0 +1,293 @@
+const Alexa = require('ask-sdk-core');
+const config = require('./config.json');
+const bringApi = require('bring-shopping');
+
+var bring = undefined;
+
+async function getBring() {
+    if (bring === undefined) {
+        bring = new bringApi({ mail: config.bringProperties.email, password: config.bringProperties.password });
+        await bring.login();
+    }
+
+    return bring;
+}
+
+function normalize(text) {
+    return text.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase();
+}
+
+function parseItem(translation, item) {
+    let translations = {};
+    Object.keys(translation).forEach(function (key) {
+        translations[normalize(translation[key])] = key;
+    });
+
+    var terms = normalize(item).split(" ");
+    for (var i = 0; i < terms.length; i++) {
+        var search = terms.slice(0, terms.length - i).join(" ");
+        if (search in translations) {
+            return {
+                "name": translations[search],
+                "spec": (i > 0 ? terms.slice(i + 1).join(" ") : "")
+            };
+        }
+
+        if (search.endsWith("s")) {
+            search = search.substring(0, search.length - 1);
+            if (search in translations) {
+                return {
+                    "name": translations[search],
+                    "spec": terms.slice(i + 2).join(" ")
+                };
+            }
+        }
+
+        search = terms.slice(i, terms.length).join(" ");
+        if (search in translations) {
+            return {
+                "name": translations[search],
+                "spec": terms.slice(0, i).join(" ")
+            };
+        }
+
+        if (search.endsWith("s")) {
+            search = search.substring(0, search.length - 1);
+            if (search in translations) {
+                return {
+                    "name": translations[search],
+                    "spec": terms.slice(0, i).join(" ")
+                };
+            }
+        }
+    }
+
+    return {
+        "name": item,
+        "spec": ""
+    }
+}
+
+const LaunchRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
+    },
+    async handle(handlerInput) {
+
+        const speakOutput = config.locales.launch;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const GetItemsIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'GetItemsIntent';
+    },
+    async handle(handlerInput) {
+
+
+        var speakOutput = config.locales.error;
+
+        try {
+            bring = await getBring();
+            const translations = await bring.loadTranslations(config.bringProperties.locale);
+            const items = await bring.getItems(config.bringProperties.listUUID);
+
+            if (items.purchase.length <= 0) {
+                speakOutput = config.locales.empty;
+            } else {
+                const itemNames = items.purchase
+                    .map(item => (translations[item.name] || item.name) + (item.specification ? " " + item.specification : ""))
+                    .sort();
+                const itemsEnumeration = itemNames.slice(0, -1).join(', ') + (itemNames.length > 1 ? " y " : '') + itemNames.slice(-1)
+                speakOutput = config.locales.list + itemsEnumeration;
+            }
+        } catch (err) {
+            speakOutput = config.locales.error;
+        }
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+const AddItemIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AddItemIntent';
+    },
+    async handle(handlerInput) {
+
+        var speakOutput = config.locales.error;
+
+        try {
+            const itemText = Alexa.getSlotValue(handlerInput.requestEnvelope, 'producto');
+            bring = await getBring();
+            const translations = await bring.loadTranslations(config.bringProperties.locale);
+            const item = parseItem(translations, itemText);
+            console.log(item);
+            await bring.saveItem(config.bringProperties.listUUID, item.name, item.spec);
+            speakOutput = `${config.locales.add} ${itemText}`;
+        } catch (err) {
+            console.log(err);
+            speakOutput = config.locales.error;
+        }
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+const RemoveItemIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'RemoveItemIntent';
+    },
+    async handle(handlerInput) {
+
+        var speakOutput = config.locales.error;
+
+        try {
+            const item = Alexa.getSlotValue(handlerInput.requestEnvelope, 'producto');
+            bring = await getBring();
+            await bring.removeItem(config.bringProperties.listUUID, item);
+            speakOutput = `${config.locales.remove} ${item}`;
+        } catch (err) {
+            speakOutput = config.locales.error;
+        }
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+
+const HelpIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'You can say hello to me! How can I help?';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+const CancelAndStopIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
+                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Goodbye!';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * FallbackIntent triggers when a customer says something that doesn’t map to any intents in your skill
+ * It must also be defined in the language model (if the locale supports it)
+ * This handler can be safely added but will be ingnored in locales that do not support it yet 
+ * */
+const FallbackIntentHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
+            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.FallbackIntent';
+    },
+    handle(handlerInput) {
+        const speakOutput = 'Sorry, I don\'t know about that. Please try again.';
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+/* *
+ * SessionEndedRequest notifies that a session was ended. This handler will be triggered when a currently open 
+ * session is closed for one of the following reasons: 1) The user says "exit" or "quit". 2) The user does not 
+ * respond or says something that does not match an intent defined in your voice model. 3) An error occurs 
+ * */
+const SessionEndedRequestHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
+    },
+    handle(handlerInput) {
+        console.log(`~~~~ Session ended: ${JSON.stringify(handlerInput.requestEnvelope)}`);
+        // Any cleanup logic goes here.
+        return handlerInput.responseBuilder.getResponse(); // notice we send an empty response
+    }
+};
+/* *
+ * The intent reflector is used for interaction model testing and debugging.
+ * It will simply repeat the intent the user said. You can create custom handlers for your intents 
+ * by defining them above, then also adding them to the request handler chain below 
+ * */
+const IntentReflectorHandler = {
+    canHandle(handlerInput) {
+        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
+    },
+    handle(handlerInput) {
+        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
+        const speakOutput = `You just triggered ${intentName}`;
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
+            .getResponse();
+    }
+};
+/**
+ * Generic error handling to capture any syntax or routing errors. If you receive an error
+ * stating the request handler chain is not found, you have not implemented a handler for
+ * the intent being invoked or included it in the skill builder below 
+ * */
+const ErrorHandler = {
+    canHandle() {
+        return true;
+    },
+    handle(handlerInput, error) {
+        const speakOutput = 'Sorry, I had trouble doing what you asked. Please try again.';
+        console.log(`~~~~ Error handled: ${JSON.stringify(error)}`);
+
+        return handlerInput.responseBuilder
+            .speak(speakOutput)
+            .reprompt(speakOutput)
+            .getResponse();
+    }
+};
+
+/**
+ * This handler acts as the entry point for your skill, routing all request and response
+ * payloads to the handlers above. Make sure any new handlers or interceptors you've
+ * defined are included below. The order matters - they're processed top to bottom 
+ * */
+exports.handler = Alexa.SkillBuilders.custom()
+    .addRequestHandlers(
+        LaunchRequestHandler,
+        GetItemsIntentHandler,
+        AddItemIntentHandler,
+        RemoveItemIntentHandler,
+        HelpIntentHandler,
+        CancelAndStopIntentHandler,
+        FallbackIntentHandler,
+        SessionEndedRequestHandler,
+        IntentReflectorHandler)
+    .addErrorHandlers(
+        ErrorHandler)
+    .withCustomUserAgent('sample/hello-world/v1.2')
+    .lambda();
